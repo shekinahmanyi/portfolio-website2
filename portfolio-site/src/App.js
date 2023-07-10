8df0bcfe03815417be4024080ffb54b75d9eacb8
shekinahmanyi <shekinahmanyi@gmail.com>
2023-07-10 21:29:18 +0100

    Added the other routes

--- portfolio-site/src/App.js
+++ portfolio-site/src/App.js
@@ -3,6 +3,11 @@ import { BrowserRouter as Router,Routes,Route} from "react-router-dom";
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import Footer from './components/Footer';
+import About from './pages/About';
+import Skills from './pages/Skills';
+import Projects from './pages/Projects';
+import Blog from './pages/Blog';
+import Contact from './pages/Contact';
 
 function App() {
   return (
@@ -10,6 +15,11 @@ function App() {
       <Navbar/>
       <Routes> 
         <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/skills" element={<Skills />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/blog" element={<Blog/>} />
+        <Route path="/contact" element={<Contact/>} />
       </Routes>
    <Footer/>
     </Router>
