0dedac349c5a981e0a1eac5991da8e63caa8d5d1
shekinahmanyi <shekinahmanyi@gmail.com>
2023-07-10 11:22:18 +0100

    arranged the setup configuarations

--- portfolio-site/src/App.js
+++ portfolio-site/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react'
+//import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div>
-      
+      <Navbar/>
     </div>
   )
 }
