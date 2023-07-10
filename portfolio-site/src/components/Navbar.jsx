63bafd5ebc5f09fd4f3d57057eda0c5db40502a6
shekinahmanyi <shekinahmanyi@gmail.com>
2023-07-10 11:30:50 +0100

    Navbar

--- portfolio-site/src/components/Navbar.jsx
+++ portfolio-site/src/components/Navbar.jsx
@@ -6,7 +6,7 @@ function Navbar() {
     <nav className='navbar bg-white text-neutral-content fixed top-0 inset-x-0 z-50 shadow-lg'>
       <div className="container mx-auto px-6 flex items-center justify-between">
         <div className="px-5 text-2xl mb-6 mt-6">
-          <span className="text-green-500 tracking-tight">
+          <span className="text-blue-500 tracking-tight">
             Sheki<span className="text-gray-800 font-mono">Dev</span>
             <FaLink className="inline pr-2 md:inline-block text-3xl" />
           </span>
