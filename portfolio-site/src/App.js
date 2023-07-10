3cf1702a7b1f8903ea51df3633bffdb1545bdd91
shekinahmanyi <shekinahmanyi@gmail.com>
2023-07-10 17:06:14 +0100

    Update Routes

--- portfolio-site/src/App.js
+++ portfolio-site/src/App.js
@@ -1,11 +1,17 @@
 import React from 'react'
-import { BrowserRouter as Router} from "react-router-dom";
+import { BrowserRouter as Router,Routes,Route} from "react-router-dom";
 import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <Router>
       <Navbar/>
+      <Routes> 
+        <Route path="/" element={<Home />} />
+      </Routes>
+   <Footer/>
     </Router>
   )
 }
