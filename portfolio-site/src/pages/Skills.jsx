8f2ccadfeac510def5241a5d2215d823f4591e22
shekinahmanyi <shekinahmanyi@gmail.com>
2023-07-11 10:40:42 +0100

    images and adjustments

--- portfolio-site/src/pages/Skills.jsx
+++ portfolio-site/src/pages/Skills.jsx
@@ -1,22 +1,57 @@
 import React from 'react';
 import reactimage from '../images/reactimage.png';
+import node from '../images/node.png';
+import bootsrapimage from '../images/bootsrapimage.png';
+import php from '../images/php.png';
+import git from '../images/git.png';
+import mongodb from '../images/mongodb.png';
+import firebase from '../images/firebase.png';
+import flutter from '../images/flutter.png'
 
 function Skills() {
   return (
-    <div className="bg-blue-950 min-h-screen flex items-center justify-center">
-      <h1 className="text-white text-center p-8 font-nunito mb-72">Tools / Technologies</h1>
+    <div className="bg-blue-950 min-h-screen flex flex-col items-center justify-center">
+      <h1 className="text-white text-center text-2xl p-8 font-nunito mb-4 mt-16">Tools / Technologies</h1>
 
-      <div className="grid grid-cols-3 gap-8 font-nunito mr-40">
+      <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-4 gap-8 font-nunito mb-8 font-bold">
         <div className="bg-black bg-opacity-20 p-2 rounded-lg flex flex-col items-center">
-          <img src={reactimage} alt="React" className="w-32 h-32 object-contain" />
+          <img src={reactimage} alt="React" className="w-32 h-32 object-contain p-8" />
+          <p className="text-white mt-2">React</p>
         </div>
 
         <div className="bg-black bg-opacity-20 p-2 rounded-lg flex flex-col items-center">
-          <img src={reactimage} alt="React" className="w-32 h-32 object-contain" />
+          <img src={node} alt="Node.js" className="w-32 h-32 object-contain" />
+          <p className="text-white mt-2">Node.js</p>
         </div>
 
         <div className="bg-black bg-opacity-20 p-2 rounded-lg flex flex-col items-center">
-          <img src={reactimage} alt="React" className="w-32 h-32 object-contain" />
+          <img src={bootsrapimage} alt="Bootstrap" className="w-32 h-32 object-contain" />
+          <p className="text-white mt-2">Bootstrap</p>
+        </div>
+
+        <div className="bg-black bg-opacity-20 p-2 rounded-lg flex flex-col items-center">
+          <img src={php} alt="PHP" className="w-32 h-32 object-contain p-4" />
+          <p className="text-white mt-2">PHP</p>
+        </div>
+
+        <div className="bg-black bg-opacity-20 p-2 rounded-lg flex flex-col items-center">
+          <img src={git} alt="Git" className="w-32 h-32 object-contain p-4" />
+          <p className="text-white mt-2">Git</p>
+        </div>
+
+        <div className="bg-black bg-opacity-20 p-2 rounded-lg flex flex-col items-center">
+          <img src={mongodb} alt="MongoDB" className="w-32 h-32 object-contain" />
+          <p className="text-white mt-2">MongoDB</p>
+        </div>
+
+        <div className="bg-black bg-opacity-20 p-2 rounded-lg flex flex-col items-center">
+          <img src={firebase} alt="Firebase" className="w-32 h-32 object-contain" />
+          <p className="text-white mt-2">Firebase</p>
+        </div>
+
+        <div className="bg-black bg-opacity-20 p-2 rounded-lg flex flex-col items-center">
+          <img src={flutter} alt="Flutter" className="w-32 h-32 object-contain p-4" />
+          <p className="text-white mt-2">Flutter</p>
         </div>
       </div>
     </div>
