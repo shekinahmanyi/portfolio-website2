91d2b1ebf7027a59c10fe0c775b513056f6a0399
shekinahmanyi <shekinahmanyi@gmail.com>
2023-07-11 22:41:32 +0100

    updated the link

--- portfolio-site/src/pages/Home.jsx
+++ portfolio-site/src/pages/Home.jsx
@@ -4,7 +4,7 @@ import { FaDownload } from "react-icons/fa";
 
 function Home() {
   const resumeUrl =
-    "https://drive.google.com/file/d/1PfIwsQIAWW3-R86Ut8KunbvnvTHMeMc7/view?usp=sharing"; // Replace with the direct URL to your resume
+    "https://drive.google.com/file/d/1AK_o6heXsa-WOqLOcajJNPNTYVoaw6m-/view?usp=sharing"; // Replace with the direct URL to your resume
 
   return (
     <div className="bg-blue-950 min-h-screen flex items-center justify-center">
