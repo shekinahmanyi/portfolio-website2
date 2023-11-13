b21aa55ec983e2e04695d10acba956efe0dbcce7
shekinahmanyi <shekinahmanyi@gmail.com>
2023-11-13 13:43:05 +0100

    made changes to the contact page

--- portfolio-site/src/pages/Contact.jsx
+++ portfolio-site/src/pages/Contact.jsx
@@ -11,15 +11,18 @@ function Contact() {
   return (
     <div className="bg-blue-950 min-h-screen flex items-center justify-center">
       <div className="container flex flex-col items-center font-nunito">
-        <h1 className="text-3xl text-blue-400 text-center font-bold">
+        <h1 className="text-3xl text-blue-400 text-center font-bold mt-6">
           Contact Me!
         </h1>
-        <p className="text-white text-center text-base p-4">
-          Do you have a project for me to work on? or an Idea?
-          One More thing is that I do not bite!!
-          <br /> Please Reach Out!😆
+        <p className="text-white text-center text-base font-mono p-6">
+          I am eager to contribute to your team and make a positive impact.   <br />
+          Do you have any projects or ideas that you would like me to
+          work on?   <br /><br />
+          I am open to new challenges and I am confident that I can
+          deliver high-quality work.<br /> I am excited to hear from you and learn
+          more about your team's goals.
         </p>
-        <div className="max-w-md bg-white p-6 rounded-lg shadow-md mt-2 mb-4">
+        <div className="w-3/5 h-96 bg-white p-6 rounded-lg shadow-md mt-2 mb-4">
           <p className="font-bold text-center text-2xl mb-4">
             Send me a Message!
           </p>
@@ -39,7 +42,7 @@ function Contact() {
               href="https://github.com/shekinahmanyi"
               target="_blank"
               rel="noopener noreferrer"
-              className="bg-blue-400 rounded-full p-2"
+              className="bg-blue-700 rounded-full p-4"
             >
               <FaGithub />
             </a>
@@ -47,7 +50,7 @@ function Contact() {
               href="https://twitter.com/ShekinahManyi"
               target="_blank"
               rel="noopener noreferrer"
-              className="bg-blue-400 rounded-full p-2"
+              className="bg-blue-700 rounded-full p-4"
             >
               <FaTwitter />
             </a>
@@ -55,7 +58,7 @@ function Contact() {
               href="https://www.linkedin.com/in/shekinah-manyi-849a21210/"
               target="_blank"
               rel="noopener noreferrer"
-              className="bg-blue-400 rounded-full p-2"
+              className="bg-blue-700 rounded-full p-4"
             >
               <FaLinkedin />
             </a>
@@ -65,7 +68,7 @@ function Contact() {
           href="https://www.buymeacoffee.com/shekinahmanyi"
           target="_blank"
           rel="noopener noreferrer"
-          className="transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 bg-yellow-500 hover:bg-yellow-600 text-white py-2 px-4 rounded-full flex items-center space-x-4 mt-4"
+          className="transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 bg-green-500 text-white text-lg font-medium px-4 py-2 rounded-md mt-4"
         >
           <span>Buy Me a Coffee</span>
         </a>
