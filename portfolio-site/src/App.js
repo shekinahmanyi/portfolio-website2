f80647c8de2d4396be53df6d039b494bedca43b3
shekinahmanyi <shekinahmanyi@gmail.com>
2024-08-20 17:27:01 +0100

    Added stuffs

--- portfolio-site/src/App.js
+++ portfolio-site/src/App.js
@@ -15,7 +15,7 @@ function App() {
   useEffect(() => {
     const timer = setTimeout(() => {
       setIsLoading(false);
-    }, 5000);
+    }, 6000);
 
     return () => clearTimeout(timer);
   }, []);
--- portfolio-site/src/components/Loading.jsx
+++ portfolio-site/src/components/Loading.jsx
@@ -2,8 +2,10 @@ import React from "react";
 
 const Loading = () => {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-slate-900">
-      <div className="w-16 h-16 border-8 border-t-blue-700 border-gray-200 rounded-full animate-spin"></div>
+    <div className="min-h-screen flex flex-col items-center justify-center bg-gradient-to-r from-purple-900 via-indigo-800 to-blue-900">
+      <div className="w-16 h-16 border-8 border-t-indigo-400 border-gray-200 rounded-full animate-spin"></div>
+      <p className="text-xl font-nunito font-bold text-white mt-4">It is Great to have you here!!🫣</p>
+      <p  className="text-xl font-nunito font-bold text-white mt-4">Be Patient🫠</p>
     </div>
   );
 };
