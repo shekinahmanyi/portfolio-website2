b4538787197bacf55bba51332027b0e7be51a735
shekinahmanyi <shekinahmanyi@gmail.com>
2025-12-02 10:35:30 +0300

    Update description in index.html and modify loading text in Loading.jsx

--- portfolio-site/src/components/Loading.jsx
+++ portfolio-site/src/components/Loading.jsx
@@ -4,8 +4,9 @@ const Loading = () => {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-gradient-to-r from-purple-900 via-indigo-800 to-blue-900">
       <div className="w-16 h-16 border-8 border-t-indigo-400 border-gray-200 rounded-full animate-spin"></div>
-      <p className="text-xl font-nunito font-bold text-white mt-4">It is Great to have you here!!🫣</p>
-      <p  className="text-xl font-nunito font-bold text-white mt-4">Be Patient🫠</p>
+      <p className="text-xl font-nunito font-bold text-white mt-4">
+        Loading...
+      </p>
     </div>
   );
 };
